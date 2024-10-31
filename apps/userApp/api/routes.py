87087e1dd9b20308b1b3e5c209c80f4ef4ef776b
Kairoz <cristianquiroz2311@gmail.com>
2024-10-31 02:14:25 -0300

    Nose Que cambios

--- apps/userApp/api/routes.py
+++ apps/userApp/api/routes.py
@@ -1,12 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views.userView import UserViewSet
 from .views.profileView import ProfileViewSet
 from .views.customGroupView import CustomGroupViewSet
 from .views.groupUserView import GroupUserViewSet
 
 router = DefaultRouter()
 
-router.register(r'user', UserViewSet, basename='user')
 router.register(r'profile', ProfileViewSet, basename='profile')
 router.register(r'customGroup', CustomGroupViewSet, basename='customGroup')
 router.register(r'groupUser', GroupUserViewSet, basename='groupUser')
