268df5bdbc516b6febe90e92e60a7e83e44034d2
Zurita <matiaszurita931@gmail.com>
2024-11-17 12:43:09 -0300

    Agregue el signals de academic

--- apps/userApp/signals.py
+++ apps/userApp/signals.py
@@ -1,9 +1,8 @@
-from .models import Profile
-from django.contrib.auth.models import User
+from .models import Profile, UserAccount
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=UserAccount)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
