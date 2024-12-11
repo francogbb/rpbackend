d254cac1eb14b6b378b829073a3dd8dd027db064
Zurita <matiaszurita931@gmail.com>
2024-12-10 22:54:35 -0300

    View para cambiar contrasena

--- apps/userApp/api/views/customUserView.py
+++ apps/userApp/api/views/customUserView.py
@@ -1,7 +1,10 @@
-from ..serializers.userSerializers import UserCreateSerializer, UserSerializerProf, UserSerializerCustomRegister
+from ..serializers.userSerializers import UserCreateSerializer, UserSerializerProf, UserSerializerCustomRegister, PasswordUpdateSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from ...models import UserAccount
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
 
 
 class CustomUserView(viewsets.ModelViewSet):
@@ -20,3 +23,19 @@ class CustomUserRegisterEditView(viewsets.ModelViewSet):
     queryset = UserAccount.objects.all()
     serializer_class = UserSerializerCustomRegister
     permission_classes = [ AllowAny ]
+    
+    
+class PasswordUpdateView(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=["put"], url_path="change-password")
+    def change_password(self, request):
+        print("Request data:", request.data)  # Depura los datos recibidos
+        serializer = PasswordUpdateSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            user = request.user
+            user.set_password(serializer.validated_data['password'])
+            user.save()
+            return Response({"detail": "Password updated successfully."}, status=status.HTTP_200_OK)
+        print("Serializer errors:", serializer.errors)  # Imprime los errores del serializer
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
