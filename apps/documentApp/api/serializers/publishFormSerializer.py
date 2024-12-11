4be3bff84f752a9479916c0fc5625db42817489f
Zurita <matiaszurita931@gmail.com>
2024-12-11 01:41:21 -0300

    Cambie el serializer del publish form y cambie la url de una estadistica

--- apps/documentApp/api/serializers/publishFormSerializer.py
+++ apps/documentApp/api/serializers/publishFormSerializer.py
@@ -4,6 +4,8 @@ from ...models import PublishForm
 class PublishFormSerializer(serializers.ModelSerializer):
     document_title = serializers.SerializerMethodField()
     teacher_name = serializers.SerializerMethodField()
+    area = serializers.SerializerMethodField()
+    area_name = serializers.SerializerMethodField()
 
     class Meta:
         model = PublishForm
@@ -15,6 +17,8 @@ class PublishFormSerializer(serializers.ModelSerializer):
             'teacher_guide',
             'document_title',
             'teacher_name',
+            'area',
+            'area_name'
         ]
 
     def get_document_title(self, obj):
@@ -26,6 +30,16 @@ class PublishFormSerializer(serializers.ModelSerializer):
         # Obtiene el nombre del profesor guía asociado al PublishForm
         teacher_guide = obj.teacher_guide  # Obtiene el objeto Profile del Teacher Guide
         return f'{teacher_guide.first_name} {teacher_guide.last_name}' if teacher_guide else None
+    def get_area(self, obj):
+        # Obtiene el área asociada al documento del PublishForm
+        document = obj.document  # Obtiene el objeto Document relacionado con el PublishForm
+        area = document.area if document else None  # Accede al área del documento
+        return area.id 
+    def get_area_name(self, obj):
+        # Obtiene el área asociada al documento del PublishForm
+        document = obj.document  # Obtiene el objeto Document relacionado con el PublishForm
+        area = document.area if document else None  # Accede al área del documento
+        return area.area_name  # Asumiendo que el área tiene un campo `name`
 
 
 class PublishFormAcceptSerializer(serializers.ModelSerializer):
